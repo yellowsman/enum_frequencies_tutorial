db0cd12a60e6d3da9cc943ad5e5896b2d160d37d
ShigeruIto <sigesigeito@gmail.com>
2021-09-22 10:19:09 +0000

    Initial commit

---
+++ lib/enumfrequenciestutorial.ex
@@ -0,0 +1,18 @@
+defmodule Enumfrequenciestutorial do
+  @moduledoc """
+  Documentation for `Enumfrequenciestutorial`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Enumfrequenciestutorial.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/enumfrequenciestutorial_test.exs
@@ -0,0 +1,8 @@
+defmodule EnumfrequenciestutorialTest do
+  use ExUnit.Case
+  doctest Enumfrequenciestutorial
+
+  test "greets the world" do
+    assert Enumfrequenciestutorial.hello() == :world
+  end
+end
